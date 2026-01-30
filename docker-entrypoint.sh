4a8b286778613b2f593079d4081c2a6d50498e3f
onicoode <onicoode@gmail.com>
2026-01-30 22:18:12 +0800

    Update one cont for render plus migrate

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,8 +13,8 @@ until php -r "
 try {
     new PDO(
         getenv('DB_CONNECTION') === 'pgsql'
-            ? 'pgsql:host='.getenv('DB_HOST').';port='.getenv('DB_PORT')
-            : 'mysql:host='.getenv('DB_HOST').';port='.getenv('DB_PORT'),
+            ? 'pgsql:host='.getenv('DB_HOST').';port='.getenv('DB_PORT').';dbname='.getenv('DB_DATABASE')
+            : 'mysql:host='.getenv('DB_HOST').';port='.getenv('DB_PORT').';dbname='.getenv('DB_DATABASE'),
         getenv('DB_USERNAME'),
         getenv('DB_PASSWORD')
     );
@@ -36,6 +36,10 @@ echo "Caching config..."
 php artisan config:cache || true
 php artisan route:cache || true
 php artisan view:cache || true
+
+echo "Configuring nginx port..."
+
+sed -i "s/__PORT__/${PORT}/g" /etc/nginx/conf.d/default.conf
 
 echo "Starting services..."
 
